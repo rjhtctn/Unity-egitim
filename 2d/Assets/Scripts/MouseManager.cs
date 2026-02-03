bce8c1aa7303bc45df49dca45c1a870ce2fb0514
Rojhat Çetin <201903327+rjhtctn@users.noreply.github.com>
2026-02-04 00:35:21 +0300

    Mouse pozisyonunu alma.

--- 2d/Assets/Scripts/MouseManager.cs
+++ 2d/Assets/Scripts/MouseManager.cs
@@ -4,17 +4,9 @@ public class MouseManager : MonoBehaviour
 {
     void Update()
     {
-        // 0 = mouse sol
-        // 1 = mouse sað
-        // 2 = mouse scroll
         if (Input.GetMouseButtonDown(0))
         {
-            Debug.Log("Mouse sol tuþ týklandý.");
-        }
-
-        if (Input.GetMouseButton(1))
-        {
-            Debug.Log("Sað tuþa basýlýyor.");
+            Debug.Log(Camera.main.ScreenToWorldPoint(Input.mousePosition));
         }
     }
 }
